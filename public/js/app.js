c0a39862f58e4624ab928bf4ab00a1f3e60d8611
Andromeda <hannalauth@gmail.com>
2019-04-09 23:45:41 -0700

    mvp styling complete still need to add hover and scrollspy effects

--- public/js/app.js
+++ public/js/app.js
@@ -1,38 +1,47 @@
-$('#submit').on('click', function(){
-    event.preventDefault()
-    
-    var firstName = $('#first-name').val().trim();
-    var lastName = $('#last-name').val().trim();
-    var item = $('#item').val().trim();
+$(document).ready(function () {
 
-    if(firstName !== "" && lastName !== "" && item !== ""){
-        $.post('/api/newAttendee', {
-            firstName: firstName,
-            lastName: lastName,
-            item: item
-        }).then(function(response){
-            console.log(response)
-        });
 
-        setTimeout(function(){window.location = '/'}, 500);
-        
-    } else {
-        $('#error-message').show()
-    }    
+    $(document).on('click', "#submit", function (event) {
+
+        event.preventDefault();
+
+        var firstName = $('#first-name').val().trim();
+        var lastName = $('#last-name').val().trim();
+        var item = $('#item').val().trim();
+
+        if (firstName !== "" && lastName !== "" && item !== "") {
+            $.post('/api/newAttendee', {
+                firstName: firstName,
+                lastName: lastName,
+                item: item
+            }).then(function (response) {
+                console.log(response)
+            });
+
+            setTimeout(function () { window.location = '/' }, 500);
+
+        } else {
+            $('#error-message').show()
+        }
+    })
+
+
+    // FIXED TABLE HEADING
+    // Change the selector if needed
+    var $table = $('table'),
+        $bodyCells = $table.find('tbody tr:first').children(),
+        colWidth;
+
+    // Get the tbody columns width array
+    colWidth = $bodyCells.map(function () {
+        return $(this).width();
+    }).get();
+
+    // Set the width of thead columns
+    $table.find('thead tr').children().each(function (i, v) {
+        $(v).width(colWidth[i]);
+    });
+
 })
 
 
-// Change the selector if needed
-var $table = $('table'),
-    $bodyCells = $table.find('tbody tr:first').children(),
-    colWidth;
-
-// Get the tbody columns width array
-colWidth = $bodyCells.map(function() {
-    return $(this).width();
-}).get();
-
-// Set the width of thead columns
-$table.find('thead tr').children().each(function(i, v) {
-    $(v).width(colWidth[i]);
-});
